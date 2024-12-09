66c725b479666bf91af80fab2c280d05828f849f
Ondrej Zelina <ondrej.webgmail.com>
2024-12-09 20:47:30 +0100

    Pridal jsem admin panel pro user s invoice

---
+++ app/Filament/User/Resources/Payment/InvoiceResource/Pages/EditInvoice.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\User\Resources\Payment\InvoiceResource\Pages;
+
+use App\Filament\User\Resources\Payment\InvoiceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditInvoice extends EditRecord
+{
+    protected static string $resource = InvoiceResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
