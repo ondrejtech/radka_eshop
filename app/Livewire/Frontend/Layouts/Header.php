298f87fe266d162dfa20f99bd8a5d6e620ea53d3
Ondrej Zelina <ondrej.webgmail.com>
2024-12-09 18:51:54 +0100

    Pridal jsem navbar dynamicky

--- app/Livewire/Frontend/Layouts/Header.php
+++ app/Livewire/Frontend/Layouts/Header.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire\Frontend\Layouts;
 
+use App\Models\Category\Category;
 use Livewire\Component;
 
 class Header extends Component
 {
+    public $category;
+    public function mount()
+    {
+        $this->category = Category::take(4)->get();
+    }
     public function render()
     {
-        return view('livewire.frontend.layouts.header');
+        return view('livewire.frontend.layouts.header',[
+            'category' => $this->category
+        ]);
     }
 }
